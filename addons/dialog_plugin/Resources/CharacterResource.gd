55187882059e22af27420ea75b0749bdfc382a8d
AnidemDex <dav.medrin@gmail.com>
2021-05-01 14:34:26 -0500

    update bubble node.
    Added a generic texture for dialog nodes

--- addons/dialog_plugin/Resources/CharacterResource.gd
+++ addons/dialog_plugin/Resources/CharacterResource.gd
@@ -6,7 +6,7 @@ export(String) var name:String = ""
 export(String) var display_name:String setget ,_get_display_name
 export(bool) var display_name_bool:bool = false
 export(Color) var color:Color = Color.white
-export(bool) var default_speaker:bool = false
+export(bool) var default_speaker:bool = false # should be removed
 export(String, MULTILINE) var description:String = ""
 # Array of DialogPortraitResource
 var portraits = PortraitArray.new() setget _set_portraits
