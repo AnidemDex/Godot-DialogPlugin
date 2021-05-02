f3076b0c3eccd5d16ce1a8c134251f18bd816f48
AnidemDex <dav.medrin@gmail.com>
2021-05-02 10:59:27 -0500

    Try WAT test and github actions

---
+++ addons/WAT/core/assertions/assertion.gd
@@ -0,0 +1,73 @@
+extends Reference
+
+var success: bool
+var expected: String = "NULL"
+var result: String
+var notes: String = "No Notes"
+var context
+
+static func _result(success: bool, expected: String, actual: String, context: String, notes: String = "No Notes"):
+	return {
+			 "success": success,
+			 "expected": expected,
+			 "actual": actual,
+			 "context": context
+			}
+		
+static func type2str(property) -> String:
+	match typeof(property):
+		TYPE_NIL:
+			return "null"
+		TYPE_BOOL:
+			return "bool"
+		TYPE_INT:
+			return "int"
+		TYPE_REAL:
+			return "float"
+		TYPE_STRING:
+			return "String"
+		TYPE_VECTOR2:
+			return "Vector2"
+		TYPE_RECT2:
+			return "Rect2"
+		TYPE_VECTOR3:
+			return "Vector3"
+		TYPE_TRANSFORM2D:
+			return "Transform2D"
+		TYPE_PLANE:
+			return "Plane"
+		TYPE_QUAT:
+			return "Quat"
+		TYPE_AABB:
+			return "AABB"
+		TYPE_BASIS:
+			return "Basis"
+		TYPE_TRANSFORM:
+			return "Transform"
+		TYPE_COLOR:
+			return "Color"
+		TYPE_NODE_PATH:
+			return "NodePath"
+		TYPE_RID:
+			return "RID"
+		TYPE_OBJECT:
+			return "Object"
+		TYPE_DICTIONARY:
+			return "Dictionary"
+		TYPE_ARRAY:
+			return "Array"
+		TYPE_RAW_ARRAY:
+			return "PoolByteArray"
+		TYPE_INT_ARRAY:
+			return "PoolIntArray"
+		TYPE_REAL_ARRAY:
+			return "PoolRealArray"
+		TYPE_STRING_ARRAY:
+			return "PoolStringArray"
+		TYPE_VECTOR2_ARRAY:
+			return "PoolVector2Array"
+		TYPE_VECTOR3_ARRAY:
+			return "PoolVector3Array"
+		TYPE_COLOR_ARRAY:
+			return "PoolColorArray"
+	return "OutOfBounds"
---
+++ addons/WAT/core/double/script_writer.gd
@@ -0,0 +1,47 @@
+extends Reference
+
+
+func write(double) -> String:
+	var source: String = ""
+	source += _extension_to_string(double)
+	source += "\nconst WATRegistry = []\n"
+	if double.base_methods.has("_init"):
+		source += _constructor_to_string(double.base_methods["_init"].arguments)
+
+	for name in double.methods:
+		var m = double.methods[name]
+		source += _method_to_string(double.get_instance_id(), m)
+	for klass in double.klasses:
+		source += _inner_class(klass)
+	source = source.replace(",)", ")")
+	return source
+	
+func _extension_to_string(double) -> String:
+	if double.is_built_in:
+		return 'extends %s' % double.klass
+	if double.inner_klass != "":
+		return 'extends "%s".%s\n' % [double.klass, double.inner_klass]
+	return 'extends "%s"\n' % double.klass
+	
+func _constructor_to_string(parameters: String) -> String:
+	var constructor: String = ""
+	constructor += "\nfunc _init(%s).(%s):" % [parameters, parameters]
+	constructor += "\n\tpass\n"
+	return constructor
+
+func _method_to_string(id: int, method: Object) -> String:
+	var text: String
+	text += "{keyword}func {name}({args_with_defaults}):"
+	text += "\n\tvar args = [{args}]"
+	text += "\n\tvar method = WATRegistry[0].method({id}, '{name}')"
+	text += "\n\tmethod.add_call(args)"
+	text += "\n\tif method.executes(args):"
+	text += "\n\t\treturn .{name}({args})"  # We may want to add a retval check here
+	text += "\n\treturn method.primary(args)\n\n"
+	text = text.format({"id": id, "keyword": method.keyword, 
+						"name": method.name, "args_with_defaults": method.args_with_defaults, 
+						"args": method.args})
+	return text
+
+func _inner_class(klass: Dictionary) -> String:
+	return "\nclass %s extends '%s'.%s:\n\tconst PLACEHOLDER = 0" % [klass.name, klass.director.klass, klass.name]
