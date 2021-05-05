3f35c1d110b395207a5a390929b29f9f99ac1f84
AnidemDex <dav.medrin@gmail.com>
2021-05-05 01:00:19 -0500

    Update DialogEditor translations

--- addons/dialog_plugin/Editor/EditorView.gd
+++ addons/dialog_plugin/Editor/EditorView.gd
@@ -3,11 +3,18 @@ extends Control
 
 const DialogUtil = preload("res://addons/dialog_plugin/Core/DialogUtil.gd")
 const DialogDB = preload("res://addons/dialog_plugin/Core/DialogDatabase.gd")
+const TranslationService = preload("res://addons/dialog_plugin/Other/translation_service/translation_service.gd")
 
 const CharacterEditorScene = preload("res://addons/dialog_plugin/Editor/Views/character_editor/CharacterEditorView.tscn")
 const TimelineEditorScene = preload("res://addons/dialog_plugin/Editor/Views/timeline_editor/TimelineEditorView.tscn")
 
 var _editor_view:Control
+
+func _ready() -> void:
+	# Be careful when using this. It could block the main thread for a few seconds
+	# Need to be improved
+	TranslationService.translate_node_recursively(self)
+
 
 func _on_ToolBar_character_selected(character) -> void:
 	if _editor_view:
