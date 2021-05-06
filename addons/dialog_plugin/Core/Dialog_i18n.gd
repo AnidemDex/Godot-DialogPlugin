344dacf7cb6b9433720bc53b464d24229f76ce6b
AnidemDex <dav.medrin@gmail.com>
2021-05-06 10:11:31 -0500

    Add translations to the editor

--- addons/dialog_plugin/Core/Dialog_i18n.gd
+++ addons/dialog_plugin/Core/Dialog_i18n.gd
@@ -1,4 +1,4 @@
-#tool
+tool
 
 const TranslationService = preload("res://addons/dialog_plugin/Other/translation_service/translation_service.gd")
 const DialogResources = preload("res://addons/dialog_plugin/Core/DialogResources.gd")
@@ -7,6 +7,7 @@ static func load_editor_translations() -> void:
 	var editor_i18n:Array = (load(DialogResources.EDITOR_i18n_PATH) as DialogDatabaseResource).resources.get_resources()
 	for translation in editor_i18n:
 		TranslationService.add_translation(translation)
+
 
 static func remove_editor_translations() -> void:
 	var editor_i18n:Array = (load(DialogResources.EDITOR_i18n_PATH) as DialogDatabaseResource).resources.get_resources()
--- addons/dialog_plugin/plugin_script.gd
+++ addons/dialog_plugin/plugin_script.gd
@@ -3,6 +3,7 @@ extends EditorPlugin
 
 const DialogResources = preload("res://addons/dialog_plugin/Core/DialogResources.gd")
 const EditorView_Scene = preload("res://addons/dialog_plugin/Editor/EditorMainNode.tscn")
+const Dialog_i18n = preload("res://addons/dialog_plugin/Core/Dialog_i18n.gd")
 
 var _editor_view
 var _parts_inspector
@@ -12,12 +13,11 @@ func _enter_tree() -> void:
 	if not load(DialogResources.CONFIGURATION_PATH).enabled:
 		return
 	DialogResources.verify_resource_directories()
-	_parts_inspector = load("res://addons/dialog_plugin/Core/DialogInspector.gd").new()
-	_translation_inspector = load("res://addons/dialog_plugin/Other/translation_service/translation_inspector.gd").new()
+	_add_editor_translations()
+	
 	_editor_view = EditorView_Scene.instance()
 	
-	add_inspector_plugin(_parts_inspector)
-	add_inspector_plugin(_translation_inspector)
+#	_add_editor_inspector_plugins()
 	
 	get_editor_interface().get_editor_viewport().add_child(_editor_view)
 	
@@ -33,13 +33,11 @@ func _ready() -> void:
 func _exit_tree() -> void:
 	if _editor_view:
 		_editor_view.queue_free()
-	if _parts_inspector:
-		remove_inspector_plugin(_parts_inspector)
-	if _translation_inspector:
-		remove_inspector_plugin(_translation_inspector)
 	_editor_view = null
-	_parts_inspector = null
-	_translation_inspector = null
+	
+	_remove_editor_inspector_plugins()
+	_remove_editor_translations()
+
 
 
 func has_main_screen() -> bool:
@@ -61,3 +59,31 @@ func get_plugin_icon():
 func make_visible(visible: bool) -> void:
 	if _editor_view:
 		_editor_view.visible = visible
+
+
+func _add_editor_inspector_plugins() -> void:
+	_parts_inspector = load("res://addons/dialog_plugin/Core/DialogInspector.gd").new()
+	_translation_inspector = load("res://addons/dialog_plugin/Other/translation_service/translation_inspector.gd").new()
+	add_inspector_plugin(_parts_inspector)
+	add_inspector_plugin(_translation_inspector)
+
+
+func _remove_editor_inspector_plugins() -> void:
+	if _parts_inspector:
+		remove_inspector_plugin(_parts_inspector)
+	if _translation_inspector:
+		remove_inspector_plugin(_translation_inspector)
+	
+	_parts_inspector = null
+	_translation_inspector = null
+
+
+func _add_editor_translations() -> void:
+	load("res://addons/dialog_plugin/Database/Editor_i18n.tres").scan_resources_folder()
+	Dialog_i18n.load_editor_translations()
+
+
+func _remove_editor_translations() -> void:
+	Dialog_i18n.remove_editor_translations()
+	_editor_view = null
+	pass
