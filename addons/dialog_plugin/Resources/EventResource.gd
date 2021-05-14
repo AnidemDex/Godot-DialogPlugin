abf795e327112fdd8bc2d6f71505d230809b3234
AnidemDex <dav.medrin@gmail.com>
2021-05-14 13:23:34 -0500

    Add custom event resource

--- addons/dialog_plugin/Resources/EventResource.gd
+++ addons/dialog_plugin/Resources/EventResource.gd
@@ -5,7 +5,7 @@ extends Resource
 const TranslationService = preload("res://addons/dialog_plugin/Other/translation_service/translation_service.gd")
 
 signal event_started(event_resource)
-signal event_finished(event_resource)
+signal event_finished(event_resource, jump_to_next_event)
 
 export(bool) var skip:bool = false
 
