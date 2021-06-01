91d84de81110be6e287aa68f805f29d6b83e3ada
AnidemDex <dav.medrin@gmail.com>
2021-06-01 17:44:47 -0500

    Update CharacterLeave event

--- addons/dialog_plugin/Nodes/editor_event_nodes/character_event/leave_event_node/leave_event_node.gd
+++ addons/dialog_plugin/Nodes/editor_event_nodes/character_event/leave_event_node/leave_event_node.gd
@@ -1,30 +1,11 @@
 tool
-extends DialogEditorEventNode
-
-export(NodePath) var CharacterBtn_path:NodePath
-
-onready var character_button_node:OptionButton = get_node(CharacterBtn_path)
+extends "res://addons/dialog_plugin/Nodes/editor_event_nodes/character_event/character_event_node_template.gd"
 
 func _ready() -> void:
-	return
 	if base_resource:
-		character_button_node.generate_items()
-		_update_node_values()
+		emit_signal("timeline_requested", self)
 
 
 func _update_node_values() -> void:
-	var _base:DialogCharacterLeaveEvent = base_resource
-	var _char:DialogCharacterResource = _base.character
-	
-	if _char:
-		character_button_node.select_item_by_resource(_char)
-
-
-func _on_CharactersButton_item_selected(index: int) -> void:
-	var _char_metadata = character_button_node.get_selected_metadata()
-	if _char_metadata is Dictionary:
-		base_resource.character = (_char_metadata as Dictionary).get("character", null)
-	else:
-		base_resource.character = null
-	_save_resource()
-	_update_node_values()
+	if base_resource.character:
+		character_button_node.select_item_by_resource(base_resource.character)
