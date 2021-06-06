4cec98277f198ef57fb9b8c699a3472007eacb6f
AnidemDex <dav.medrin@gmail.com>
2021-06-06 18:50:55 -0500

    Update SetEvent

--- addons/dialog_plugin/Resources/Events/SetEvent/SetEvent.gd
+++ addons/dialog_plugin/Resources/Events/SetEvent/SetEvent.gd
@@ -7,24 +7,38 @@ var variable_value
 
 func _init() -> void:
 	# Uncomment resource_name line if you want to display a name in the editor
-	#resource_name = "<your_event_name>"
+	resource_name = "SetVariableEvent"
 
 	# Uncomment event_editor_scene_path line and replace it with your custom DialogEditorEventNode scene
 	#event_editor_scene_path = "res://path/to/your/editor/node/scene.tscn"
 
 	# Uncomment skip line if you want your event jump directly to next event 
 	# at finish or not (false by default)
-	#skip = false
-	pass
+	skip = true
+
 
 func execute(caller:DialogBaseNode) -> void:
 	# Parent function must be called at the start
 	.execute(caller)
+	# Try to convert variable.name to variable:name in case using object
+	variable_name = variable_name.replace(".", ":")
+	# Load variables resource
 	var _variable_resource = load(VARIABLES_PATH)
-	_variable_resource.set_value()
+	var _variables:Dictionary = _variable_resource.variables
+	var _variable_properties_to_modify:Array = Array(variable_name.split(":"))
+	var _variable_name:String = _variable_properties_to_modify[0]
 	
+	var _variable = _variables.get(_variable_name, NAN)
+	match typeof(_variable):
+		TYPE_OBJECT:
+			var property_path = NodePath(variable_name)
+			_variable.set_indexed(property_path.get_concatenated_subnames(), variable_value)
 
-	# There goes your event code.
+		TYPE_REAL:
+			if is_nan(_variable):
+				push_warning("La variable que intentabas modificar no existe, la añadiremos a las variables.")
+			continue
+		_:
+			_variable_resource.set_value(_variable_name, variable_value)
 
-	# Notify that you end this event
 	finish()
