72f5fe3339c3f2e3e097fbc53fbf042392037046
AnidemDex <dav.medrin@gmail.com>
2021-06-24 14:24:13 -0500

    Update comment event script
    Add the path to the node scene

--- addons/dialog_plugin/Resources/Events/MiscelaneousEvent/Comment/CommentEvent.gd
+++ addons/dialog_plugin/Resources/Events/MiscelaneousEvent/Comment/CommentEvent.gd
@@ -6,7 +6,7 @@ export(String, MULTILINE) var text:String = ""
 
 func _init() -> void:
 	resource_name = "Comment"
-	#event_editor_scene_path = "res://path/to/your/editor/node/scene.tscn"
+	event_editor_scene_path = "res://addons/dialog_plugin/Nodes/editor_event_nodes/comment_event_node/comment_event_node.tscn"
 	skip = true
 
 
