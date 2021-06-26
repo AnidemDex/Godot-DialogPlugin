58978b389619f9ac7e38f712614c0d901ba53570
AnidemDex <dav.medrin@gmail.com>
2021-06-26 18:54:21 -0500

    Add TextAudio event

---
+++ addons/dialog_plugin/Nodes/editor_event_nodes/text_event_node/text_with_audio_node/text_with_audio.gd
@@ -0,0 +1,2 @@
+tool
+extends "res://addons/dialog_plugin/Nodes/editor_event_nodes/text_event_node/text_event_node.gd"
---
+++ addons/dialog_plugin/Resources/Events/TextEvent/TextAudioEvent/TextAudioEvent.gd
@@ -0,0 +1,24 @@
+tool
+# class_name <your_event_class_name_here>
+extends DialogTextEvent
+
+func _init() -> void:
+	# Uncomment resource_name line if you want to display a name in the editor
+	resource_name = "TextWithAudio"
+
+	# Uncomment event_editor_scene_path line and replace it with your custom DialogEditorEventNode scene
+	#event_editor_scene_path = "res://path/to/your/editor/node/scene.tscn"
+
+	# Uncomment skip line if you want your event jump directly to next event 
+	# at finish or not (false by default)
+	#skip = false
+
+
+func execute(caller:DialogBaseNode) -> void:
+	# Parent function must be called at the start
+	.execute(caller)
+
+	# There goes your event code.
+
+	# Notify that you end this event
+	finish()
