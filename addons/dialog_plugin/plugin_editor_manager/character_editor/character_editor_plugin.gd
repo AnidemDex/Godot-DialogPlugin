55bd33d88e25b9c861c7497ada885e4a10ee6dd8
AnidemDex <dav.medrin@gmail.com>
2021-07-05 15:41:21 -0500

    Update character editor manager logs

--- addons/dialog_plugin/plugin_editor_manager/character_editor/character_editor_plugin.gd
+++ addons/dialog_plugin/plugin_editor_manager/character_editor/character_editor_plugin.gd
@@ -2,16 +2,21 @@ tool
 extends EditorPlugin
 
 const DialogResources = preload("res://addons/dialog_plugin/Core/DialogResources.gd")
+const DialogUtil = preload("res://addons/dialog_plugin/Core/DialogUtil.gd")
 const PLUGIN_NAME = "Character Editor Manager"
 
 var _character_editor_view:Control
 var _dock_button:ToolButton
+
+func get_class(): return "CharacterEditorManager"
+
 
 func _init() -> void:
 	name = PLUGIN_NAME
 
 
 func _enter_tree() -> void:
+	DialogUtil.Logger.print_debug(self, PLUGIN_NAME+" initialized.")
 	_character_editor_view = load(DialogResources.CHARACTER_EDITOR_PATH).instance()
 	_dock_button = add_control_to_bottom_panel(_character_editor_view, "CharacterEditor")
 	_dock_button.visible = false
@@ -24,6 +29,7 @@ func handles(object: Object) -> bool:
 
 
 func edit(object: Object) -> void:
+	DialogUtil.Logger.print_debug(self, "Modifying {0}".format([object.resource_path]))
 	_character_editor_view.base_resource = object
 
 
