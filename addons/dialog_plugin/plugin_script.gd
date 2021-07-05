e9d333712153cfa6dc27b084a6850182eb41d36c
AnidemDex <dav.medrin@gmail.com>
2021-07-05 15:26:25 -0500

    Update plugin script logs

--- addons/dialog_plugin/plugin_script.gd
+++ addons/dialog_plugin/plugin_script.gd
@@ -4,6 +4,7 @@ extends EditorPlugin
 const PLUGIN_NAME = "Dialog Editor Plugin"
 const DialogResources = preload("res://addons/dialog_plugin/Core/DialogResources.gd")
 const Dialog_i18n = preload("res://addons/dialog_plugin/Core/Dialog_i18n.gd")
+const DialogUtil = preload("res://addons/dialog_plugin/Core/DialogUtil.gd")
 
 var interface:EditorInterface = get_editor_interface()
 
@@ -19,18 +20,63 @@ var modules = [
 func _init() -> void:
 	name = PLUGIN_NAME
 
+func get_class(): return "DialogEditorPlugin"
+
 
 func _enter_tree() -> void:
 #	_add_editor_translations()
-	
 #	_add_editor_inspector_plugins()
 	pass
 
 
 func enable_plugin() -> void:
+	enable_all_modules()
+	
+	if not can_proceed():
+		force_plugin_disable()
+		return
+	
 	show_alert()
+	DialogUtil.Logger.print_info(self, "Plugin enabled.")
+
+
+func enable_all_modules() -> void:
 	for module in modules:
 		interface.set_plugin_enabled(module, true)
+	DialogUtil.Logger.print_debug(self, "All modules enabled... probably.")
+
+
+func disable_all_modules() -> void:
+	for module in modules:
+		if interface.is_plugin_enabled(module):
+			interface.set_plugin_enabled(module, false)
+	DialogUtil.Logger.print_debug(self, "All modules disabled.")
+
+
+func verify_all_modules() -> int:
+	for module in modules:
+		var _enabled = interface.is_plugin_enabled(module)
+		if not _enabled:
+			DialogUtil.Logger.print_info(self, "Module <<{module}>> is not enabled. Maybe something went wrong when the plugin was being initialized?".format({"module":module.get_file()}))
+			return ERR_SCRIPT_FAILED
+	
+	return OK
+
+
+func verify_plugin() -> int:
+	if verify_all_modules() != OK:
+		return ERR_SCRIPT_FAILED
+	return OK
+
+
+func can_proceed() -> bool:
+	return verify_plugin() == OK
+
+
+func force_plugin_disable() -> void:
+	DialogUtil.Logger.print_error(self, "The plugin found an error. It'll be disabled.")
+	disable_plugin()
+	interface.set_plugin_enabled("dialog_plugin", false)
 
 
 func show_alert() -> void:
@@ -38,12 +84,12 @@ func show_alert() -> void:
 	interface.add_child(alert_popup)
 	alert_popup.connect("hide", alert_popup, "queue_free")
 	alert_popup.popup_centered()
+	DialogUtil.Logger.print_debug(self, "Showing startup alert.")
 
 
 func disable_plugin() -> void:
-	for module in modules:
-		if interface.is_plugin_enabled(module):
-			interface.set_plugin_enabled(module, false)
+	disable_all_modules()
+	DialogUtil.Logger.print_info(self, "Plugin disabled.")
 
 
 func _exit_tree() -> void:
