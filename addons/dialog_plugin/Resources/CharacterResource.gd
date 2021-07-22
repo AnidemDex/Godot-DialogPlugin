ea30a5cc1a3538d8fd10fcd90a5602ccf7124399
AnidemDex <dav.medrin@gmail.com>
2021-07-22 14:04:06 -0500

    Change CharacterResource
    related to $146

--- addons/dialog_plugin/Resources/CharacterResource.gd
+++ addons/dialog_plugin/Resources/CharacterResource.gd
@@ -2,67 +2,85 @@ tool
 extends Resource
 class_name DialogCharacterResource
 
-export(String) var name:String = "" setget ,_get_name
-export(String) var display_name:String setget ,_get_display_name
-export(Color) var color:Color = Color.white
-export(Texture) var icon:Texture = null
-export(AudioStream) var blip_sound:AudioStream = null
-# Array of DialogPortraitResource
-var portraits = PortraitArray.new() setget _set_portraits
+const DialogUtil = preload("res://addons/dialog_plugin/Core/DialogUtil.gd")
 
-func _get_name() -> String:
+export(String) var name:String = "" setget set_name,get_name
+export(String) var display_name:String setget set_display_name,get_display_name
+export(Color) var color:Color = Color.white setget set_color
+export(Texture) var icon:Texture = null setget set_icon
+var blip_sounds:Array = []
+var portraits:Array = []
+
+func add_portrait(portrait:DialogPortraitResource) -> void:
+	if not portrait in portraits:
+		portraits.append(portrait)
+		emit_changed()
+
+
+func add_blip_sound(blip_sound:AudioStream) -> void:
+	if not blip_sound in blip_sounds:
+		blip_sounds.append(blip_sound)
+		emit_changed()
+
+
+func remove_portrait(portrait:DialogPortraitResource) -> void:
+	if portrait in portraits:
+		portraits.erase(portrait)
+		emit_changed()
+
+
+
+func remove_blip_sound(blip_sound:AudioStream) -> void:
+	if blip_sound in blip_sounds:
+		blip_sounds.erase(blip_sound)
+		emit_changed()
+
+
+func set_name(value:String) -> void:
+	name = value
+	emit_changed()
+
+
+func set_display_name(value:String) -> void:
+	display_name = value
+	emit_changed()
+
+
+func set_color(value:Color) -> void:
+	color = value
+	emit_changed()
+
+
+func set_icon(value:Texture) -> void:
+	icon = value
+	emit_changed()
+
+
+func get_name() -> String:
 	if name == "":
 		name = resource_path.get_file().replace("."+resource_path.get_extension(),"")
 		resource_name = name
+		emit_changed()
 		
 	return name
 
-func _get_display_name() -> String:
+
+func get_display_name() -> String:
 	if display_name:
 		return display_name
 	else:
 		return name
 
-func get_good_name(with_name:String="") -> String:
-	var _good_name = with_name
-	
-	if self.display_name:
-		return self.display_name
-	else:
-		if _good_name.begins_with("res://"):
-			_good_name = _good_name.replace("res://", "")
-		if _good_name.ends_with(".tres"):
-			_good_name = _good_name.replace(".tres", "")
-		_good_name = _good_name.capitalize()
-		return _good_name
-
-
-func _set_portraits(value) -> void:
-	portraits = value
-	if not value:
-		portraits = PortraitArray.new()
-
 
 func _get_property_list() -> Array:
 	var properties:Array = []
-	properties.append(
-		{
-			"name":"portraits_number",
-			"type":TYPE_INT,
-			"hint":PROPERTY_HINT_NONE,
-			"usage":PROPERTY_USAGE_NO_INSTANCE_STATE | PROPERTY_USAGE_EDITOR,
-		}
-	)
-	properties.append(
-		{
-			"name":"portraits",
-			"type":TYPE_OBJECT,
-			"hint":PROPERTY_HINT_RESOURCE_TYPE,
-			"usage":PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_NOEDITOR
-		}
-	)
+	var portrait_hint:Dictionary = DialogUtil.get_property_dict("portraits_number", TYPE_INT, PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NO_INSTANCE_STATE | PROPERTY_USAGE_EDITOR)
+	var portraits_property:Dictionary = DialogUtil.get_property_dict("portraits", TYPE_ARRAY, PROPERTY_HINT_NONE, "", PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_NOEDITOR)
+	properties.append(portrait_hint)
+	properties.append(portraits_property)
 	return properties
+
 
 func _get(property: String):
 	if property == "portraits_number":
-		return portraits.get_resources().size()
+		return portraits.size()
