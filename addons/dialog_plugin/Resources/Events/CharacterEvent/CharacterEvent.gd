44e199eb39eba65ab6cfbb23019283351cab8bf2
AnidemDex <dav.medrin@gmail.com>
2021-07-22 16:48:59 -0500

    Update character event

--- addons/dialog_plugin/Resources/Events/CharacterEvent/CharacterEvent.gd
+++ addons/dialog_plugin/Resources/Events/CharacterEvent/CharacterEvent.gd
@@ -8,7 +8,7 @@ var selected_portrait:int = -1
 func get_selected_portrait() -> DialogPortraitResource:
 	var _selected_portrait:DialogPortraitResource = null
 	if character and selected_portrait != -1:
-		_selected_portrait = character.portraits.get_resources()[selected_portrait]
+		_selected_portrait = character.portraits[selected_portrait]
 	return _selected_portrait
 
 func _get_property_list() -> Array:
