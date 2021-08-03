6ec679c091b54e5a45303935219f7856aae871f2
AnidemDex <dav.medrin@gmail.com>
2021-08-03 15:50:11 -0500

    Remove debug print

--- addons/dialog_plugin/Nodes/editor_event_node/event_node_name/event_node_name.gd
+++ addons/dialog_plugin/Nodes/editor_event_node/event_node_name/event_node_name.gd
@@ -9,7 +9,6 @@ onready var name_label:Label = get_node(NameLabel_path) as Label
 onready var panel_style:StyleBoxFlat = get_stylebox("panel")
 
 func update_node_values() -> void:
-	print_debug("Updating name")
 	if base_resource:
 		name_label.text = base_resource.event_name
 		panel_style.bg_color = base_resource.event_color
