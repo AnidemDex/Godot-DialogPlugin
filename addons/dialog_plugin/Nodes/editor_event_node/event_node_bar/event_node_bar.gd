2ee5e618e85f1125cefef17ebcfaa3c800c4d5ba
AnidemDex <dav.medrin@gmail.com>
2021-08-13 11:10:45 -0500

    Add a method to drop data over the event node
    Fix #20

--- addons/dialog_plugin/Nodes/editor_event_node/event_node_bar/event_node_bar.gd
+++ addons/dialog_plugin/Nodes/editor_event_node/event_node_bar/event_node_bar.gd
@@ -11,6 +11,23 @@ var is_focused:bool = false
 onready var event_node_preview:Control = get_node(Preview_path) as Control
 onready var event_node_properties:Control = get_node(Properties_path) as Control
 
+onready var viewport:Viewport = get_viewport()
+
+func _process(delta: float) -> void:
+	# Hello, developer from the future. It's me Dex, trying to explain what
+	# happens here (because this is an ugly method to do):
+	# We mix the rect of the node with child's rects to create a bigger rect area.
+	# With that area, we verify if this node has the focus, and if we are
+	# draggin something.
+	# Finally, we verify if the mouse is outside the event node expanded rect
+	var preview_rect:Rect2 = event_node_preview.get_rect()
+	var event_rect:Rect2 = event_node_properties.get_rect()
+	var combined_rect:Rect2 = get_rect().merge(preview_rect).merge(event_rect)
+	if is_focused and viewport.gui_is_dragging():
+		if not combined_rect.has_point(get_local_mouse_position()):
+			release_focus()
+
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
@@ -64,3 +81,11 @@ func hide_properties() -> void:
 		event_node_preview.call("_after_collapse")
 	if event_node_properties.has_method("_after_collapse"):
 		event_node_properties.call("_after_collapse")
+
+
+func can_drop_data(position: Vector2, data) -> bool:
+	var _can_drop_data = base_resource.get("can_drop_data")
+	if _can_drop_data:
+		grab_focus()
+		update()
+	return false
