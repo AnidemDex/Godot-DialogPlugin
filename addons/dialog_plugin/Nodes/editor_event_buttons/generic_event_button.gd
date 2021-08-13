bd00fe4d0cb4e70ff6ad8198be6b591a553ddc10
AnidemDex <dav.medrin@gmail.com>
2021-08-13 09:18:32 -0500

    Add event node template to event button
    Fix #25

--- addons/dialog_plugin/Nodes/editor_event_buttons/generic_event_button.gd
+++ addons/dialog_plugin/Nodes/editor_event_buttons/generic_event_button.gd
@@ -6,6 +6,8 @@ signal being_dragged
 # must be a DialogEventResource
 var event_resource:Script
 
+var event_node_template_scene:PackedScene = preload("res://addons/dialog_plugin/Nodes/editor_event_node/event_node_template.tscn")
+
 func _ready() -> void:
 	if not event_resource:
 		return
@@ -27,12 +29,17 @@ func _pressed() -> void:
 func get_drag_data(position):
 	emit_signal("being_dragged")
 	var data = event_resource.new()
-	var drag_preview_node:Control = data.get_event_editor_node()
+	var drag_preview_node:Control = event_node_template_scene.instance() as Control
+	drag_preview_node.set("base_resource", data)
+	drag_preview_node.set("event_index", -1)
+	
 	drag_preview_node.size_flags_horizontal = Control.SIZE_FILL
 	drag_preview_node.size_flags_vertical = Control.SIZE_FILL
 	drag_preview_node.anchor_right = 0
 	drag_preview_node.anchor_bottom = 0
 	drag_preview_node.rect_size = Vector2(50,50)
 	drag_preview_node.rect_min_size = Vector2(50,50)
+	
 	set_drag_preview(drag_preview_node)
+	drag_preview_node.call("update_event_node_values")
 	return data
