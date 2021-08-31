e099ca83ae71bb7a6645941b84fe8162ab9354ab
AnidemDex <dav.medrin@gmail.com>
2021-08-31 16:36:42 -0500

    Reestructure timeline selector
    Probably fix a bug related to not displaying timelines inside events

--- addons/dialog_plugin/Nodes/editor_event_node/event_property_nodes/resource_property/timeline_selector/timeline_selector.gd
+++ addons/dialog_plugin/Nodes/editor_event_node/event_property_nodes/resource_property/timeline_selector/timeline_selector.gd
@@ -2,16 +2,13 @@ tool
 extends "res://addons/dialog_plugin/Nodes/editor_event_node/event_property_nodes/event_property.gd".PControl
 
 const EventDisplayer = preload("res://addons/dialog_plugin/Editor/Views/timeline_editor/events_displayer.gd")
+const TimelineSelector = preload("res://addons/dialog_plugin/Nodes/misc/timeline_selector/timeline_selector.gd")
 
-export(NodePath) var EventDisplayer_path:NodePath
 export(NodePath) var TmlnSelector_path:NodePath
 export(NodePath) var PropertyName_path:NodePath
-export(NodePath) var TimelineContainer_path:NodePath
 
 onready var property_label:Label = get_node(PropertyName_path) as Label
-onready var timeline_selector:Button = get_node(TmlnSelector_path) as Button
-onready var event_displayer:EventDisplayer = get_node(EventDisplayer_path) as EventDisplayer
-onready var timeline_container:Container = get_node(TimelineContainer_path) as Container
+onready var timeline_selector:TimelineSelector = get_node(TmlnSelector_path) as TimelineSelector
 
 func update_node_values() -> void:
 	update_property_label()
@@ -19,35 +16,12 @@ func update_node_values() -> void:
 	var timeline:DialogTimelineResource = null
 	var timeline_path:String
 	timeline = base_resource.get(used_property)
-	if timeline:
-		timeline_path = timeline.resource_path
-		if "::" in timeline_path:
-			event_displayer.timeline_resource = timeline
-			event_displayer.force_reload()
-			show_timeline()
-		else:
-			event_displayer.timeline_resource = null
-			event_displayer.unload_events()
-			hide_timeline()
-	else:
-		push_error("For some reason, this property '{0}' doesn't had any timeline".format([used_property]))
+	timeline_selector.select_resource(timeline)
 
 
 func update_property_label() -> void:
 	property_label.text = used_property.capitalize()
 
 
-func hide_timeline() -> void:
-	timeline_container.hide()
-
-func show_timeline() -> void:
-	timeline_container.show()
-
-
-func _on_ResourceSelector_resource_selected(resource:Resource) -> void:
-	if resource is DialogTimelineResource:
-		base_resource.set(used_property, resource)
-		hide_timeline()
-	if resource == null:
-		base_resource.set(used_property, DialogTimelineResource.new())
-		show_timeline()
+func _on_TimelineSelector_resource_selected(resource:DialogTimelineResource) -> void:
+	base_resource.set(used_property, resource)
