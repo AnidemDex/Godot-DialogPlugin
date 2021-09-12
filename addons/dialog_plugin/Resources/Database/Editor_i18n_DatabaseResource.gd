190c319f7240a504d4c676bd7e6bcda1750d3ce0
AnidemDex <dav.medrin@gmail.com>
2021-09-11 23:04:59 -0500

    Fix typo in database script

--- addons/dialog_plugin/Resources/Database/Editor_i18n_DatabaseResource.gd
+++ addons/dialog_plugin/Resources/Database/Editor_i18n_DatabaseResource.gd
@@ -2,12 +2,12 @@ tool
 #extends DialogDatabaseResource
 
 var DialogUtil := load("res://addons/dialog_plugin/Core/DialogUtil.gd")
-var resource_type
-var scanned_directory
 
 # Deprecated
 func _init() -> void:
 	return
+	var resource_type
+	var scanned_directory
 	resource_type = Translation
 	
 	# Hardcoded since nobody is going to change this path, right?
@@ -16,6 +16,8 @@ func _init() -> void:
 # Deprecated
 func add(item:Translation) -> void:
 	return
+	var resources
+	var DialogResources
 	DialogUtil.Logger.print(self, ["Adding a translation: ", item.resource_path])
 	if resources.get_resources().has(item):
 		push_warning("A resource is already there")
@@ -23,11 +25,11 @@ func add(item:Translation) -> void:
 		var _idx = _r_array.find(item)
 		if _idx != -1:
 			_r_array[_idx] = item
-			save(DialogResources.EDITOR_i18n_PATH)
+#			save(DialogResources.EDITOR_i18n_PATH)
 			emit_signal("changed")
 		return
 	
 	(resources as ResourceArray).add(item)
-	save(DialogResources.EDITOR_i18n_PATH)
+#	save(DialogResources.EDITOR_i18n_PATH)
 	emit_signal("changed")
 	pass
