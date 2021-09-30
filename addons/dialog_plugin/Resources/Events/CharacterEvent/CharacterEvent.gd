d85d6b5d676bf941210f0e0a8aaf2df54614e002
AnidemDex <dav.medrin@gmail.com>
2021-09-29 20:32:33 -0500

    Add CharacterEvent documentation

--- addons/dialog_plugin/Resources/Events/CharacterEvent/CharacterEvent.gd
+++ addons/dialog_plugin/Resources/Events/CharacterEvent/CharacterEvent.gd
@@ -2,30 +2,42 @@ tool
 class_name DialogCharacterEvent
 extends DialogEventResource
 
+##
+## Base Event Class for every event related to a character
+##
+
+## The character used for this event.
 var character:DialogCharacterResource = null setget set_character
+
+## The portrait index selected for this event.
 var selected_portrait:int = 0 setget set_selected_portrait
 
 func _init() -> void:
 	event_color = Color("#4CB963")
 
 
+## Returns the portrait selected according to selected_portrait or null if none is selected.
 func get_selected_portrait() -> DialogPortraitResource:
 	var _selected_portrait:DialogPortraitResource = null
 	if character and selected_portrait != -1:
 		_selected_portrait = character.portraits[selected_portrait]
 	return _selected_portrait
 
+
 func set_character(value:DialogCharacterResource) -> void:
 	character = value
 	emit_changed()
+
 
 func set_selected_portrait(value:int) -> void:
 	selected_portrait = value
 	emit_changed()
 
+
 func _get(property: String):
 	if property == "selected_portrait_alternative_node":
 		return load("res://addons/dialog_plugin/Nodes/editor_event_node/event_property_nodes/numeric_property/portrait_list.tscn")
+
 
 func _get_property_list() -> Array:
 	var _properties:Array = []
