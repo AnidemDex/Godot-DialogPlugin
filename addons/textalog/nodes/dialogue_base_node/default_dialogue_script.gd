d49eb0a54eb21a9763b9badaa33bd6e912f71e9e
AnidemDex <dav.medrin@gmail.com>
2021-11-13 11:25:11 -0500

    readd portrait class

--- addons/textalog/nodes/dialogue_base_node/default_dialogue_script.gd
+++ addons/textalog/nodes/dialogue_base_node/default_dialogue_script.gd
@@ -1,6 +1,5 @@
-extends DialogBaseNode
+extends DialogNode
 
 # TODO: Mejora este mensaje de error
 func _ready() -> void:
-	_set_nodes_default_values()
 	push_error("If you can see this, you didn't replaced this script. Consider replacing it to make it work")
