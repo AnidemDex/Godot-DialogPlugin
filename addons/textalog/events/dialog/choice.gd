696add71883fa85722eb18fead76c4ed2b7d58cb
AnidemDex <dav.medrin@gmail.com>
2021-11-26 15:03:24 -0500

    Add choice event script.
    - This event let you choice between many options displayed by
    OptionsManager
    - A unique timeline is created per option
    - Its inner function is to append events at front of the
    current timeline

---
+++ addons/textalog/events/dialog/choice.gd
@@ -0,0 +1,114 @@
+tool
+extends Event
+
+var options:Dictionary = {}
+
+var _options_manager:OptionsManager
+
+func add_option(option_name:String) -> void:
+	options[option_name] = Timeline.new()
+	emit_changed()
+	property_list_changed_notify()
+
+
+func get_option(option_name:String) -> Timeline:
+	return options.get(option_name, null) as Timeline
+
+
+func remove_option(option_name:String) -> void:
+	var option_was_removed := options.erase(option_name)
+	if option_was_removed:
+		emit_changed()
+		property_list_changed_notify()
+
+
+func _set_option(option_name:String, value) -> void:
+	if (value == null) or not (value is Timeline):
+		value = Timeline.new()
+	
+	options[option_name] = value
+	emit_changed()
+	property_list_changed_notify()
+
+
+func _set(property: String, value) -> bool:
+	if property.begins_with("options/"):
+		var key = property.replace("options/", "")
+		_set_option(key, value)
+		return true
+	
+	return false
+
+
+func _get(property: String):
+	if property.begins_with("options/"):
+		var key = property.replace("options/","")
+		if options.has(key):
+			return options[key]
+	
+	return null
+
+
+func _on_option_selected(option) -> void:
+	var curr_tmln:Timeline = event_manager.timeline
+	var curr_queue := curr_tmln._event_queue.duplicate()
+	
+	var timeline:Timeline = options[option] as Timeline	
+	var events:Array = timeline.get_events()
+	events.invert()
+	
+	for event in events:
+		curr_queue.push_front(event)
+	curr_tmln._event_queue = curr_queue
+	finish()
+
+
+func _execute() -> void:
+	event_node = event_node as DialogNode
+	if not is_instance_valid(event_node):
+		finish()
+		return
+	
+	_options_manager = event_node.options_manager as OptionsManager
+	if not is_instance_valid(_options_manager):
+		finish()
+		return
+	
+	_options_manager.show()
+	
+	for option in options:
+		_options_manager.add_option(option)
+	
+	_options_manager.connect("option_selected", self, "_on_option_selected", [], CONNECT_ONESHOT)
+
+
+func _get_property_list() -> Array:
+	var p := []
+	
+	p.append(
+		{
+			"name":"Options",
+			"type":TYPE_NIL,
+			"usage":PROPERTY_USAGE_CATEGORY
+		}
+	)
+	for option in options:
+		p.append(
+			{
+				"name":"options/"+str(option),
+				"type":TYPE_OBJECT,
+				"usage":PROPERTY_USAGE_DEFAULT|PROPERTY_USAGE_SCRIPT_VARIABLE,
+				"hint":PROPERTY_HINT_RESOURCE_TYPE,
+				"hint_string":"Timeline"
+			}
+		)
+	
+	return p
+
+
+func _init() -> void:
+	event_color = Color("2892D7")
+	event_name = "Choice"
+	event_icon = load("res://addons/textalog/assets/icons/event_icons/question_event.png") as Texture
+	
+	options = options.duplicate()
