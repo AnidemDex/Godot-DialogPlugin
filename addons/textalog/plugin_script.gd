d2522a355bd4f2af2afb6a1502c68a9fa8ca08cd
AnidemDex <dav.medrin@gmail.com>
2021-11-27 12:24:27 -0500

    Add plugin dependency.
    This commit updates plugin_script and welcome scene to try
    to search and enable EventSystem and show custom dialog events
    in FileSystem.
    
    A bit experimental and untested feature.
    
    - Hi scene adds new methods to display meta
    - Hi scene now can use plugin script to check EventSystem status
    - Plugin script adds plugin inspector array to enable multiple inspectors
    if event system is enabled
    - Plugin script now has a method to retrieve data from configuration file
    - Plugin script can try to enable EventSystem
    - Fixes the position of version button

--- addons/textalog/plugin_script.gd
+++ addons/textalog/plugin_script.gd
@@ -3,50 +3,138 @@ extends EditorPlugin
 
 const PLUGIN_NAME = "Textalog"
 
+var inspector_script_path := []
+
 var _welcome_scene:PackedScene = load("res://addons/textalog/nodes/editor/welcome/hi.tscn")
 
 var _plugin_data := ConfigFile.new()
 var _version_button:BaseButton
 
+var _inspectors := []
+
+
+func get_plugin_data(data:String) -> String:
+	var info = _plugin_data.get_value("plugin",data, "???")
+	return info
+
 
 func get_plugin_version() -> String:
-	var _version = _plugin_data.get_value("plugin","version", "0")
-	return _version
+	return get_plugin_data("version")
 
 
-func _enter_tree() -> void:
-	_add_version_button()
+func is_event_system_enabled() -> bool:
+	var editor_interface:EditorInterface = get_editor_interface()
+	var event_system = null
+	
+	if get_tree().has_meta("EventSystem"):
+		# Mira si existe el "singleton"
+		event_system = get_tree().get_meta("EventSystem")
+	else:
+		# Sino, buscalo manualmente
+		event_system = editor_interface.get_parent().get_node_or_null("Event System")
+	return is_instance_valid(event_system)
+
+
+func enable_event_system() -> void:
+	if is_event_system_enabled():
+		return
+	var editor_interface:EditorInterface = get_editor_interface()
+	editor_interface.set_plugin_enabled("event_system_plugin", true)
+	
+	var path = "res://addons/textalog/events/"
+	var file = ".gdignore"
+	if editor_interface.is_plugin_enabled("event_system_plugin"):
+		var dir := Directory.new()
+		if dir.file_exists(path+file):
+			dir.remove(path+file)
+	else:
+		var _file := File.new()
+		_file.open(path+file,File.WRITE)
+		_file.close()
+		
+	
+	call_deferred("_remove_inspector_events")
+	call_deferred("_add_inspector_events")
+	
+	editor_interface.get_resource_filesystem().call_deferred("scan")
+	editor_interface.get_resource_filesystem().call_deferred("scan_sources")
+	editor_interface.get_resource_filesystem().call_deferred("update_script_classes")
+
+
+func _add_itself_to_editor_meta() -> void:
+	get_tree().set_meta("Textalog", self)
+
+
+func _add_inspector_events() -> void:
+	# if event system enabled
+	if not get_editor_interface().is_plugin_enabled("event_system_plugin"):
+		push_warning("Event system is not enabled")
+		return
+	for script_path in inspector_script_path:
+		var inspector_plugin_script = load(script_path)
+		var inspector_plugin:EditorInspectorPlugin = inspector_plugin_script.new()
+		add_inspector_plugin(inspector_plugin)
+		_inspectors.append(inspector_plugin)
+
+
+func _add_version_button() -> void:
+	var _v = {"version":get_plugin_version()}
+	
+	_version_button = ToolButton.new()
+	_version_button.connect("pressed", self, "_show_welcome")
+	_version_button.text = "TaL:[{version}]".format(_v)
+	_version_button.hint_tooltip = "Textalog version {version}".format(_v)
+	
+	connect("tree_exiting", _version_button, "free")
+	
+	var _new_color = _version_button.get_color("font_color")
+	_new_color.a = 0.6
+	_version_button.add_color_override("font_color", _new_color)
+	_version_button.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	
+	var _dummy := Control.new()
+	var _dock_button := add_control_to_bottom_panel(_dummy, "dummy")
+	_dock_button.get_parent().get_parent().add_child(_version_button)
+	_dock_button.get_parent().get_parent().move_child(_version_button, 1)
+	remove_control_from_bottom_panel(_dummy)
+	_dummy.free()
+
+
+func _remove_inspector_events() -> void:
+	for inspector_plugin in _inspectors:
+		remove_inspector_plugin(inspector_plugin)
+	_inspectors = []
+
+
+func _remove_itself_from_editor_meta() -> void:
+	get_tree().remove_meta("Textalog")
+
+
+func _show_welcome() -> void:
+	var _popup:Popup = _welcome_scene.instance() as Popup
+	_popup.set("_textalog_plugin", self)
+	_popup.set("_docs", get_plugin_data("docs"))
+	_popup.set("_repo", get_plugin_data("repository"))
+	_popup.set("_license", get_plugin_data("license"))
+	_popup.set("_credits", get_plugin_data("credits"))
+	_popup.connect("ready", _popup, "popup_centered_ratio", [0.4])
+	_popup.connect("popup_hide", _popup, "queue_free")
+	_popup.connect("hide", _popup, "queue_free")
+	get_editor_interface().get_base_control().add_child(_popup)
 
 
 func enable_plugin() -> void:
 	_show_welcome()
 
 
-func _add_version_button() -> void:
-	var _v = {"version":get_plugin_version()}
-	_version_button = ToolButton.new()
-	connect("tree_exiting", _version_button, "free")
-	_version_button.text = "TaL:[{version}]".format(_v)
-	_version_button.hint_tooltip = "Textalog version {version}".format(_v)
-	var _new_color = _version_button.get_color("font_color")
-	_new_color.a = 0.6
-	_version_button.add_color_override("font_color", _new_color)
-	_version_button.size_flags_horizontal = Control.SIZE_SHRINK_END|Control.SIZE_EXPAND
-	_version_button.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	
-	var _dummy := Control.new()
-	var _dock_button := add_control_to_bottom_panel(_dummy, "dummy")
-	_dock_button.get_parent().add_child(_version_button)
-	remove_control_from_bottom_panel(_dummy)
-	_dummy.free()
+func _enter_tree() -> void:
+	_add_version_button()
+	_add_itself_to_editor_meta()
 
 
-func _show_welcome() -> void:
-	var _popup:Popup = _welcome_scene.instance() as Popup
-	_popup.connect("ready", _popup, "popup_centered_ratio", [0.4])
-	_popup.connect("popup_hide", _popup, "queue_free")
-	_popup.connect("hide", _popup, "queue_free")
-	get_editor_interface().get_base_control().add_child(_popup)
+func _exit_tree() -> void:
+	_remove_inspector_events()
+	_remove_itself_from_editor_meta()
 
 
 func _init() -> void:
