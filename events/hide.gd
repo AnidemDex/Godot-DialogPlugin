68b2aae829533c9eeaf84425bd799a541f673a03
Dex <dav.medrin@gmail.com>
2021-12-30 11:02:19 -0500

    Make event set extends from EventCallFrom.
    This commit makes that events `hide`, `set` and `show` to extends from
    `EventCallFrom`, allowing using an absolute node path to set values.
    Fix #21

--- events/hide.gd
+++ events/hide.gd
@@ -1,5 +1,5 @@
 tool
-extends Event
+extends "res://addons/event_system_plugin/events/call_from.gd"
 class_name EventHide
 
 func _init() -> void:
@@ -7,8 +7,13 @@ func _init() -> void:
 	event_name = "Hide"
 	event_category = "Node"
 	event_icon = load("res://addons/event_system_plugin/assets/icons/event_icons/hidden.png") as Texture
+	method = "set"
+	args = ["visible", false]
+	event_preview_string = "{node_path}"
 
 
-func _execute() -> void:
-	event_node.set("visible", false)    
-	finish()
+func _get(property: String):
+	if property == "method_ignore":
+		return true
+	if property == "args_ignore":
+		return true
