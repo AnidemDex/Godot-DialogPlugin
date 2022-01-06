17d71bcef00283ec3f66bfd486e1b4bd00ee1851
Alex Noir <Varsash@gmail.com>
2022-01-06 17:39:16 +0300

    Optimize all Godette assets using PNGGauntlet (https://pnggauntlet.com/) Enable BBCode on IC logs Remove duplicate_logging option from ICLogs due to potentially being super laggy

--- examples/VisualNovel/ICLogs.gd
+++ examples/VisualNovel/ICLogs.gd
@@ -7,26 +7,16 @@ onready var scroll_container = $LogLayout/ScrollContainer
 onready var piece_container = $LogLayout/ScrollContainer/PieceContainer
 onready var log_layout = $LogLayout
 
-export var duplicate_logging: bool = true
-
 func clear():
 	for piece in piece_container.get_children():
 		piece.queue_free()
 
 
 func add_piece(icon: Texture, showname: String, text: String):
-	if not duplicate_logging:
-		for piece in piece_container.get_children():
-			var piece_name = piece.get_node("HBoxContainer/VBoxContainer/Name").text
-			var piece_dialog = piece.get_node("HBoxContainer/VBoxContainer/Dialog").text
-			if showname == piece_name and text == piece_dialog:
-				# This is a duplicate - don't bother logging it
-				return
-	
 	var piece: Control = piece_res.instance()
 	piece.get_node("HBoxContainer/Avatar").texture = icon
 	piece.get_node("HBoxContainer/VBoxContainer/Name").text = showname
-	piece.get_node("HBoxContainer/VBoxContainer/Dialog").text = text
+	piece.get_node("HBoxContainer/VBoxContainer/Dialog").bbcode_text = text
 	piece_container.add_child(piece)
 	yield(get_tree(), "idle_frame")
 	scroll_container.ensure_control_visible(piece)
